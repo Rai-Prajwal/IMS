2c5ffc64bdd922ef437c6ca87e704bda63b94ed2
Prajwal Rai <prajwalrai64@gmail.com>
2024-10-01 23:16:32 +0545

    LoginController

--- src/main/java/Controllers/LoginController.java
+++ src/main/java/Controllers/LoginController.java
@@ -1,5 +1,23 @@
 package Controllers;
 
-public class LoginController {
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class LoginController implements Initializable {
+	public TextField username;
+	public PasswordField password;
+	public CheckBox remember_me;
+	public Label forgot_password;
+	public Button login;
+	
+	@Override
+	public void initialize (URL url, ResourceBundle resourceBundle) {
+		
+	}
 }
