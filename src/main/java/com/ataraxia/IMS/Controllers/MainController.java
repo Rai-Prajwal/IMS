8c7a6a65f6ac9441645d002abb39b44adbd1d838
Prajwal Rai <prajwalrai64@gmail.com>
2024-10-04 22:15:50 +0545

    added logout action

--- src/main/java/com/ataraxia/IMS/Controllers/MainController.java
+++ src/main/java/com/ataraxia/IMS/Controllers/MainController.java
@@ -5,6 +5,7 @@ import javafx.scene.layout.BorderPane;
 import java.net.URL;
 import java.util.ResourceBundle;
 import com.ataraxia.IMS.Models.Model;
+import javafx.stage.Stage;
 
 public class MainController implements Initializable{
 	public BorderPane main_view;
@@ -16,10 +17,17 @@ public class MainController implements Initializable{
 			case"Register" -> main_view.setCenter(Model.getInstance().getViewFactory().getRegisterView());
 			case"FAQ" -> main_view.setCenter(Model.getInstance().getViewFactory().getFaqView());
 			case"Settings" -> main_view.setCenter(Model.getInstance().getViewFactory().getSettingsView());
-			case"Logout" -> Model.getInstance().getViewFactory().showLoginWindow();
+			case"Logout" -> handleLogout();
 			case"Report" -> main_view.setCenter(Model.getInstance().getViewFactory().getReportView());
 			default -> main_view.setCenter(Model.getInstance().getViewFactory().getHomeView());
 			}
 		});
+	}
+	
+	private void handleLogout() {
+		Model.getInstance().getViewFactory().showLoginWindow();
+		
+		Stage currentStage = (Stage) main_view.getScene().getWindow();
+		currentStage.close();
 	}
 }
