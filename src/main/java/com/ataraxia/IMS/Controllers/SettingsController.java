f5aa16fa4a3722b28786208647366ef203853293
Prajwal Rai <prajwalrai64@gmail.com>
2024-10-04 16:54:12 +0545

    added untracked files to staging area settingscontroller,css for faq and settings and fxml for faq,report,settings

---
+++ src/main/java/com/ataraxia/IMS/Controllers/SettingsController.java
@@ -0,0 +1,19 @@
+package com.ataraxia.IMS.Controllers;
+
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.scene.control.Tab;
+
+public class SettingsController implements Initializable{
+	public Tab general;
+	public Tab account;
+	public Tab appearance;
+	public Tab notifications;
+	public Tab support;
+	
+	@Override
+	public void initialize (URL url, ResourceBundle resourceBundle) {
+		
+	}
+}
