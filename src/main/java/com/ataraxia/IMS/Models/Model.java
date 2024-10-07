c0b0b25833f61cb94741a88f8241835b4371c628
Prajwal Rai <prajwalrai64@gmail.com>
2024-10-07 23:28:31 +0545

    updated code for themes to somewhat working condition

--- src/main/java/com/ataraxia/IMS/Models/Model.java
+++ src/main/java/com/ataraxia/IMS/Models/Model.java
@@ -1,23 +1,49 @@
 package com.ataraxia.IMS.Models;
 
 import com.ataraxia.IMS.Views.ViewFactory;
+import javafx.scene.Scene;
 
 public class Model {
-	private static Model model;
-	private final ViewFactory viewFactory;
-	
-	private Model() {
-		this.viewFactory = new ViewFactory();
-	}
-	
-	public static synchronized Model getInstance() {
-		if(model == null) {
-			model = new Model();
-		}
-		return model;
-	}
-	
-	public ViewFactory getViewFactory() {
-		return viewFactory; 
-	}
+    private static Model model;
+    private final ViewFactory viewFactory;
+    private String currentTheme;
+
+    private Model() {
+        this.viewFactory = new ViewFactory();
+        this.currentTheme = "/Css/darkmode.css"; // Default theme
+    }
+
+    public static synchronized Model getInstance() {
+        if (model == null) {
+            model = new Model();
+        }
+        return model;
+    }
+
+    public ViewFactory getViewFactory() {
+        return viewFactory;
+    }
+
+    public String getCurrentTheme() {
+        return currentTheme;
+    }
+
+    public void setCurrentTheme(String themeName) {
+        if (themeName.equals("Light Mode")) {
+            currentTheme = "/Css/lightmode.css";
+        } else if (themeName.equals("Dark Mode")) {
+            currentTheme = "/Css/darkmode.css";
+        }
+    }
+
+    public void applyTheme(Scene scene) {
+        if (scene != null) {
+            String themeStylesheet = getClass().getResource(currentTheme).toExternalForm();
+            
+            scene.getStylesheets().removeIf(stylesheet -> 
+                stylesheet.contains("lightmode.css") || stylesheet.contains("darkmode.css"));
+            
+            scene.getStylesheets().add(themeStylesheet);
+        }
+    }
 }
--- src/main/java/com/ataraxia/IMS/Views/ViewFactory.java
+++ src/main/java/com/ataraxia/IMS/Views/ViewFactory.java
@@ -4,108 +4,128 @@ import javafx.beans.property.StringProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene; 
 import com.ataraxia.IMS.Controllers.MainController;
+import com.ataraxia.IMS.Controllers.SettingsController;
+import com.ataraxia.IMS.Models.Model;
 
 public class ViewFactory {
-	private final StringProperty menuSwitch;
-	private AnchorPane homeView;
-	private AnchorPane registerView;
-	private AnchorPane faqView;
-	private AnchorPane settingsView;
-	private AnchorPane reportView;
-	
-	public ViewFactory() {
-		this.menuSwitch = new SimpleStringProperty("");
-	}
-	
-	public StringProperty getMenuSwitch() {
-		return menuSwitch;
-	}
-	
-	public AnchorPane getHomeView() {
-		if(homeView == null) {
-			try{
-				homeView = new FXMLLoader(getClass().getResource("/Fxml/Home.fxml")).load();
-			}catch(Exception e) {
-				e.printStackTrace();
-			}
-		}
-		return homeView;
-	}
-	
-	public AnchorPane getRegisterView() {
-		if(registerView == null) {
-			try {
-				registerView = new FXMLLoader(getClass().getResource("/Fxml/Register.fxml")).load();
-			}catch(Exception e) {
-				e.printStackTrace();
-			}
-		}
-		return registerView;
-	}
-	
-	public AnchorPane getFaqView() {
-		if(faqView == null) {
-			try {
-				faqView = new FXMLLoader(getClass().getResource("/Fxml/FAQ.fxml")).load();
-			}catch(Exception e) {
-				e.printStackTrace();
-			}
-		}
-		return faqView;
-	}
-	
-	public AnchorPane getSettingsView() {
-		if(settingsView == null) {
-			try {
-				settingsView = new FXMLLoader(getClass().getResource("/Fxml/Settings.fxml")).load();
-			}catch(Exception e) {
-				e.printStackTrace();
-			}
-		}
-		return settingsView;
-	}
-	
-	public AnchorPane getReportView() {
-		if(reportView == null) {
-			try {
-				reportView = new FXMLLoader(getClass().getResource("/Fxml/Report.fxml")).load();
-			}catch(Exception e) {
-				e.printStackTrace();
-			}
-		}
-		return reportView;
-	}
-	
-	public void showLoginWindow(){
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fxml/Login.fxml"));
-		createStage(loader);
-	}
-	
-	public void showMainWindow() {
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fxml/Main.fxml"));
-		MainController mainController = new MainController();
-		loader.setController(mainController);
-		createStage(loader);
-	}
-	
-	private void createStage(FXMLLoader loader) {
-		Scene scene = null;
-		try {
-			scene = new Scene(loader.load());
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-		Stage stage = new Stage();
-		stage.setScene(scene);
-		stage.setTitle("Institution Management System");
-		stage.show();				
-	}
-	
-	public void closeStage(Stage stage) {
-		stage.close();
-	}
-}
+    private final StringProperty menuSwitch;
+    private AnchorPane homeView;
+    private AnchorPane registerView;
+    private AnchorPane faqView;
+    private AnchorPane settingsView;
+    private AnchorPane reportView;
+    
+    public ViewFactory() {
+        this.menuSwitch = new SimpleStringProperty("");
+    }
+    
+    public StringProperty getMenuSwitch() {
+        return menuSwitch;
+    }
+    
+    public AnchorPane getHomeView() {
+        if(homeView == null) {
+            try{
+                homeView = new FXMLLoader(getClass().getResource("/Fxml/Home.fxml")).load();
+            }catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return homeView;
+    }
+    
+    public AnchorPane getRegisterView() {
+        if(registerView == null) {
+            try {
+                registerView = new FXMLLoader(getClass().getResource("/Fxml/Register.fxml")).load();
+            }catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return registerView;
+    }
+    
+    public AnchorPane getFaqView() {
+        if(faqView == null) {
+            try {
+                faqView = new FXMLLoader(getClass().getResource("/Fxml/FAQ.fxml")).load();
+            }catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return faqView;
+    }
+    
+    public AnchorPane getSettingsView() {
+        if(settingsView == null) {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fxml/Settings.fxml"));
+                settingsView = loader.load();
+                SettingsController controller = loader.getController();
+                controller.initializeTheme();
+            }catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return settingsView;
+    }
+    
+    public AnchorPane getReportView() {
+        if(reportView == null) {
+            try {
+                reportView = new FXMLLoader(getClass().getResource("/Fxml/Report.fxml")).load();
+            }catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return reportView;
+    }
+    
+    public void showLoginWindow(){
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fxml/Login.fxml"));
+        createStage(loader);
+    }
+    
+    public void showMainWindow() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Fxml/Main.fxml"));
+        MainController mainController = new MainController();
+        loader.setController(mainController);
+        createStage(loader);
+    }
+    
+    private void createStage(FXMLLoader loader) {
+        Scene scene = null;
+        try {
+            scene = new Scene(loader.load());
+            Model.getInstance().applyTheme(scene);
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.setTitle("Institution Management System");
+        
+        Model.getInstance().applyTheme(scene);
+        
+        stage.show();                
+    }
+    
+    public void closeStage(Stage stage) {
+        stage.close();
+    }
 
+    public void applyThemeToAllWindows() {
+        for (Window window : Stage.getWindows()) {
+            if (window instanceof Stage) {
+                Scene scene = ((Stage) window).getScene();
+                if (scene != null) {
+                    Model.getInstance().applyTheme(scene);
+                }
+            }
+        }
+    }
+}
